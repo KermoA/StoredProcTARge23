0147be93051bd85b6f4fa49ea06a708633b8de1b
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-05-21 15:07:57 +0300

    DbContext

--- StoredProcTARge23/Data/StoredProcDbContext.cs
+++ StoredProcTARge23/Data/StoredProcDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StoredProcTARge23.Models;
 
 
 namespace StoredProcTARge23.Data
 {
     public class StoredProcDbContext : DbContext
     {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base() { }
 
+        public DbSet<Employee> Employees { get; set; }
     }
 }
